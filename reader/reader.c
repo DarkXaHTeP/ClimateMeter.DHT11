830e7f397e8cccbea00572a482d2bffd99cdb644
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-29 00:27:39 +0200

    Added simple reader application

---
+++ reader/reader.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <dht11.h>
+
+int main( void )
+{
+	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+
+	if ( init_dht11(7) == 0 )
+    {
+        printf( "Unable to initialize dht library\n" );
+		exit( 1 );
+    }
+
+	while ( 1 )
+	{
+		if ( read_dht11_data() == 1)
+        {
+            printf("Humidity = %d.%d %% Temperature = %d.%d *C\n", get_humidity(), get_temp());
+        }
+        else
+        {
+            printf("Data is not good, skipping\n");
+        }
+        
+		delay( 1000 ); /* wait 1sec to refresh */
+	}
+
+	return(0);
+}
